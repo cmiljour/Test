71fc9084e2abac60217233c112aacd1f2a64cd22
cmiljour <cormil@gmail.com>
2017-06-21 11:02:02 -0500

    added spotify wrapper

--- auth.js
+++ auth.js
@@ -3,14 +3,12 @@
 OAuth.initialize('fy7hrBW50VLQie5JdFTyjjeLjrw');
 OAuth.popup('spotify', {cache: true}).done(function(result) {
     console.log(result);
-}).fail(function(err) {
-  //todo when the OAuth flow failed
-})   
+});
 
-$(document).ready(function(){
+
 var spotify = OAuth.create('spotify');
 var spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken('<BQBkEMVJFFRepWhbN-EDyhMymUNp4vbPudE1BJMaPq0TGG9orFo2S0BT3LQymLeCp4-xL-_YicMbHuvX929ATS88-EGoq9ZIrZCb5hcJMzjmzn7a-WduhKWbhGwNrA90w0zS2lorm939MnqF5QcoNdYFwK_QBnfflppTNYLo2wVbImTSqyLpMzcvIxNMMWmJW7IG-MmvrpPs7-qJ3FQSMfO3A8rl11FbHLFojm4iqzKKfOwHJO-7orzr_rQdmooNMmoUAZ5NH85_ucTxdE2Qsy1hd19j9Gy3>');
+spotifyApi.setAccessToken('BQBkEMVJFFRepWhbN-EDyhMymUNp4vbPudE1BJMaPq0TGG9orFo2S0BT3LQymLeCp4-xL-_YicMbHuvX929ATS88-EGoq9ZIrZCb5hcJMzjmzn7a-WduhKWbhGwNrA90w0zS2lorm939MnqF5QcoNdYFwK_QBnfflppTNYLo2wVbImTSqyLpMzcvIxNMMWmJW7IG-MmvrpPs7-qJ3FQSMfO3A8rl11FbHLFojm4iqzKKfOwHJO-7orzr_rQdmooNMmoUAZ5NH85_ucTxdE2Qsy1hd19j9Gy3');
 
 spotifyApi.getUserPlaylists()  // note that we don't pass a user id
   .then(function(data) {
@@ -18,4 +16,3 @@ spotifyApi.getUserPlaylists()  // note that we don't pass a user id
   }, function(err) {
     console.error(err);
   });
-});
