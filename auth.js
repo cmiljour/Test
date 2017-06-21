ca8c55d9201567654c481420b7e07440c0a4a60d
cmiljour <cormil@gmail.com>
2017-06-21 10:39:06 -0500

    added spotify wrapper

--- auth.js
+++ auth.js
@@ -4,8 +4,13 @@ OAuth.initialize('fy7hrBW50VLQie5JdFTyjjeLjrw');
 OAuth.redirect('spotify', 'https://cmiljour.github.io/Test/');
 
 //in your callback page (can be the same page)
-OAuth.callback('spotify').done(function(spotify) {
-  //make API calls with `google`
+// OAuth.callback('spotify').done(function(spotify) {
+//   //make API calls with `google`
+//   console.log("success");
+
+// }).fail(function(err) {
+//   //todo when the OAuth flow failed
+// })
 
   var spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken('<BQBkEMVJFFRepWhbN-EDyhMymUNp4vbPudE1BJMaPq0TGG9orFo2S0BT3LQymLeCp4-xL-_YicMbHuvX929ATS88-EGoq9ZIrZCb5hcJMzjmzn7a-WduhKWbhGwNrA90w0zS2lorm939MnqF5QcoNdYFwK_QBnfflppTNYLo2wVbImTSqyLpMzcvIxNMMWmJW7IG-MmvrpPs7-qJ3FQSMfO3A8rl11FbHLFojm4iqzKKfOwHJO-7orzr_rQdmooNMmoUAZ5NH85_ucTxdE2Qsy1hd19j9Gy3>');
@@ -16,8 +21,3 @@ spotifyApi.getUserPlaylists()  // note that we don't pass a user id
   }, function(err) {
     console.error(err);
   });
-
-}).fail(function(err) {
-  //todo when the OAuth flow failed
-})
-
