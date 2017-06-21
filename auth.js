fa64cecaedf372b571da8c3bd7af52ed138ff40f
cmiljour <cormil@gmail.com>
2017-06-21 11:21:00 -0500

    added spotify wrapper

--- auth.js
+++ auth.js
@@ -1,12 +1,12 @@
 
 
 OAuth.initialize('fy7hrBW50VLQie5JdFTyjjeLjrw');
-OAuth.popup('spotify', {cache: true}).done(function(result) {
+OAuth.popup('spotify', .done(function(result) {
     console.log(result);
 });
 
 
-var spotify = OAuth.create('spotify');
+// var spotify = OAuth.create('spotify');
 var spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken('BQCZ8XCeRbWnQVM750AT-us_sRd924BGYVXzN-oKS5PWtc0Odn…n41IBInNOUfnl8SWMHBPG6vX_uqb27cu7M-Rmf9KWaTg9jM8g');
 
