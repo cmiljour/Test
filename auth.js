ec7feec385b4cb51721eb50240cdd612f18ac167
cmiljour <cormil@gmail.com>
2017-06-21 09:25:04 -0500

    added spotify wrapper

--- auth.js
+++ auth.js
@@ -6,3 +6,12 @@ OAuth.popup('spotify').done(function(result) {
     // do some stuff with result
 })
 
+var spotifyApi = new SpotifyWebApi();
+spotifyApi.setAccessToken('<BQDFPdekt3csmqzIUhOFrfHKiVDiZ7CrY173e89SVaFOAPBEASSl7n_otDK_yotZglTPEcpprId0S87l9bRAKD6TL1wUM8vEfcJUkZ6TGCV5oGc8x-ZN45A2ZS4ZAdDCOluUPd26mAKv>');
+
+spotifyApi.searchTracks('Love')
+  .then(function(data) {
+    console.log('Search by "Love"', data);
+  }, function(err) {
+    console.error(err);
+  });
