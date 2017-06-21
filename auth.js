6e47c992f7d852346be9aab327d0a8eea48a3289
cmiljour <cormil@gmail.com>
2017-06-21 11:22:28 -0500

    added spotify wrapper

--- auth.js
+++ auth.js
@@ -7,12 +7,12 @@ OAuth.popup('spotify', .done(function(result) {
 
 
 // var spotify = OAuth.create('spotify');
-var spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken('BQCZ8XCeRbWnQVM750AT-us_sRd924BGYVXzN-oKS5PWtc0Odn…n41IBInNOUfnl8SWMHBPG6vX_uqb27cu7M-Rmf9KWaTg9jM8g');
+// var spotifyApi = new SpotifyWebApi();
+// spotifyApi.setAccessToken('BQCZ8XCeRbWnQVM750AT-us_sRd924BGYVXzN-oKS5PWtc0Odn…n41IBInNOUfnl8SWMHBPG6vX_uqb27cu7M-Rmf9KWaTg9jM8g');
 
-spotifyApi.getUserPlaylists()  // note that we don't pass a user id
-  .then(function(data) {
-    console.log('User playlists', data);
-  }, function(err) {
-    console.error(err);
-  });
+// spotifyApi.getUserPlaylists()  // note that we don't pass a user id
+//   .then(function(data) {
+//     console.log('User playlists', data);
+//   }, function(err) {
+//     console.error(err);
+//   });
